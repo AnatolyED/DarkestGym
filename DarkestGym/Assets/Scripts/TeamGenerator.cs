37ae5fe39651962fe5456ede7c1749e87e515dd5
dolaprolorap <dolaprolorapff@gmail.com>
2023-09-28 13:59:04 +1000

    fixing again merge

--- DarkestGym/Assets/Scripts/TeamGenerator.cs
+++ DarkestGym/Assets/Scripts/TeamGenerator.cs
@@ -19,34 +19,20 @@ public class TeamGenerator : MonoBehaviour
                 Debug.Log(unit.GetComponent<BaseUnit>()._scriptableObject.GetHealth);
             */
             #endregion
-
-            GameObject _unit = Instantiate(unit, position[i].position,Quaternion.identity, player.gameObject.transform);
             
-            if(_unit.TryGetComponent(out BaseUnit baseUnit))
+            Quaternion _rotation = player.GetPlayerNumber == PlayerNumber.First ? Quaternion.identity : Quaternion.Euler(0, -180, 0);
+
+            GameObject _unit = Instantiate(unit, position[i].position, _rotation, player.gameObject.transform);
+            _unit.GetComponent<BaseUnit>().SetGameManager = player.GetGameManager;
+            _unit.GetComponent<BaseUnit>().GetUnitNumber = player.GetPlayerNumber;
+            position[i] = FindCell(position[i], gameManager);
+            position[i].GetComponent<Cell>().GetUnit = _unit;
+
+            if (_unit.TryGetComponent(out BaseUnit baseUnit))
             {
                 teamList.AddUnit(baseUnit);
             }
 
-            position[i] = FindCell(position[i],gameManager);
-            position[i].GetComponent<Cell>().GetUnit = _unit;
-            
-            if (player.GetPlayerNumber == PlayerNumber.First)
-            {
-                GameObject _unit = Instantiate(unit, position[i].position, Quaternion.identity, player.gameObject.transform);
-                _unit.GetComponent<BaseUnit>().SetGameManager = player.GetGameManager;
-                _unit.GetComponent<BaseUnit>().GetUnitNumber = player.GetPlayerNumber;
-                position[i] = FindCell(position[i], gameManager);
-                position[i].GetComponent<Cell>().GetUnit = _unit;
-            }
-            else
-            {
-                GameObject _unit = Instantiate(unit, position[i].position, Quaternion.Euler(0,-180,0), player.gameObject.transform);
-                _unit.GetComponent<BaseUnit>().SetGameManager = player.GetGameManager;
-                _unit.GetComponent<BaseUnit>().GetUnitNumber = player.GetPlayerNumber;
-                position[i] = FindCell(position[i], gameManager);
-                position[i].GetComponent<Cell>().GetUnit = _unit;
-            }
-        
             i++;
         }
 
--- DarkestGym/Assets/Scripts/Units/BaseUnit.cs
+++ DarkestGym/Assets/Scripts/Units/BaseUnit.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
