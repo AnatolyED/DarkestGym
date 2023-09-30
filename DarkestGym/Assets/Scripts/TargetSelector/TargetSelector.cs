54dc4849010e7d19ad5417800752602c9b5e3a8a
dolaprolorap <dolaprolorapff@gmail.com>
2023-09-30 12:10:19 +1000

    add docs to TargetSelector

--- DarkestGym/Assets/Scripts/TargetSelector/TargetSelector.cs
+++ DarkestGym/Assets/Scripts/TargetSelector/TargetSelector.cs
@@ -2,22 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class that realize logic of selecting units (BaseUnits)
+/// </summary>
 public class TargetSelector
 {
     private static bool _targetSelectorIsActive = false;
     private Coroutine _clickListener; 
 
+    /// <summary>
+    /// Shows if another TargetSelector is active now.
+    /// If another TargetSelector is active now, new TargetSelector will 
+    /// not be created.
+    /// </summary>
     public static bool TargetSelectorIsActive => _targetSelectorIsActive;
 
+    /// <summary>
+    /// Fires when user selects unit (first param)
+    /// </summary>
     public event System.Action<BaseUnit> OnTargetSelected;
+    /// <summary>
+    /// Fires if user clicked but did not select unit
+    /// </summary>
     public event System.Action OnTargetNotSelected;
+    /// <summary>
+    /// Fires after OnTargerSelected and OnTargerNotSelected and 
+    /// finishes work of target selector
+    /// </summary>
     public event System.Action OnTargetSelectorEnd;
 
+    /// <summary>
+    /// Creates TargetSelector and launches it with default cursor
+    /// </summary>
     public TargetSelector() : this(null, Vector2.zero)
     {
         
     }
 
+    /// <summary>
+    /// Creates TargetSelector and launches it with custom cursor
+    /// </summary>
     public TargetSelector(Texture2D cursorTexture, Vector2 hotspot)
     {
         if (!TargetSelectorIsActive)
@@ -38,6 +62,9 @@ public class TargetSelector
         }
     }
 
+    /// <summary>
+    /// Forcing finish of TargetSelector
+    /// </summary>
     public void Close()
     {
         OnTargetSelectorEnd();
